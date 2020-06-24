5749e5f2faf4a6965f5dbace4721a6518828416d
Haruhiko Tano <hikoharu06@gmail.com>
2020-06-24 22:58:08 +0900

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get '/signup', to: 'users#new'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
@@ -11,4 +13,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only:[:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 # メインのサンプルユーザーを1人作成する
 User.create!(name: 'Example User',
-             email: 'example@railstutorial.org',
+             email: 'example@railstutorialabc.org',
              password: 'foobar',
              password_confirmation: 'foobar',
              admin: true,
